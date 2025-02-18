87c6b3df7a11d4d7912ac5070df515de089ebac7
TuxWare <71888789+TuxWarez@users.noreply.github.com>
2025-02-18 20:14:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#
+# TO-DO:
+# fix play_move_(direction) functions
+#
+
 import random
 import os
 
@@ -26,51 +31,82 @@ def add_item():
         else:
             check_if_free(4)
 
-def play_move_up():
+def play_move_up(arr):
     for i in range(4):
         for j in range(3):
-            if grid[j][i] == grid[j + 1][i] or grid[j + 1][i] == 0 or grid[j][i] == 0:
-                grid[j][i] += grid[j + 1][i]
-                grid[j + 1][i] = 0
+            if arr[j][i] == arr[j + 1][i] or arr[j + 1][i] == 0 or arr[j][i] == 0:
+                arr[j][i] += arr[j + 1][i]
+                arr[j + 1][i] = 0
 
-def play_move_down():
+def play_move_down(arr):
     for i in range(4):
         for j in range(3):
-            if grid[j][i] == grid[j + 1][i] or grid[j + 1][i] == 0 or grid[j][i] == 0:
-                grid[j + 1][i] += grid[j][i]
-                grid[j][i] = 0
+            if arr[j][i] == arr[j + 1][i] or arr[j + 1][i] == 0 or arr[j][i] == 0:
+                arr[j + 1][i] += arr[j][i]
+                arr[j][i] = 0
 
-def play_move_right():
+def play_move_right(arr):
     for i in range(4):
         for j in range(3):
-            if grid[i][j] == grid[i][j + 1] or grid[i][j + 1] == 0 or grid[i][j] == 0:
-                grid[i][j + 1] += grid[i][j]
-                grid[i][j] = 0
+            if arr[i][j] == arr[i][j + 1] or arr[i][j + 1] == 0 or arr[i][j] == 0:
+                arr[i][j + 1] += arr[i][j]
+                arr[i][j] = 0
 
-def play_move_left():
+def play_move_left(arr):
     for i in range(4):
         for j in range(3):
-            if grid[i][j] == grid[i][j + 1] or grid[i][j + 1] == 0 or grid[i][j] == 0:
-                grid[i][j] += grid[i][j + 1]
-                grid[i][j + 1] = 0
+            if arr[i][j] == arr[i][j + 1] or arr[i][j + 1] == 0 or arr[i][j] == 0:
+                arr[i][j] += arr[i][j + 1]
+                arr[i][j + 1] = 0
+
+def check_if_stuck():
+    x = 0
+    temp_grid = grid
+    play_move_up(temp_grid)
+    if temp_grid = grid:
+        x += 1
+    temp_grid = grid
+    play_move_down(temp_grid)
+    if temp_grid = grid:
+        x += 1
+    temp_grid = grid
+    play_move_left(temp_grid)
+    if temp_grid = grid:
+        x += 1
+    temp_grid = grid
+    play_move_right(temp_grid)
+    if temp_grid = grid:
+        x += 1
+    if x == 4:
+        return True
 
 def play_input():
-    move = input('Input direction:').lower()
+    move = input('Input direction: ').lower()
     if len(move) > 1 or move not in ALLOWED_MOVES:
         print('Try again;')
         play_input()
     if move == 'w':
-        play_move_up()
+        play_move_up(grid)
     elif move == 's':
-        play_move_down()
+        play_move_down(grid)
     elif move == 'd':
-        play_move_right()
+        play_move_right(grid)
     elif move == 'a':
-        play_move_left()
+        play_move_left(grid)
 
+def main():
+    while True:
+        os.system('clear')
+        add_item()
+        flag = check_if_stuck
+        if flag:
+            print("Game over")
+            break
+        print_grid()
+        play_input()
 
 while True:
-    os.system('clear')
-    add_item()
-    print_grid()
-    play_input()
+    grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    main()
+    if input("Do you want to play another game? [y/N] ")!="y":
+        break
